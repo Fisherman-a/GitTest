87757fb13bd0063d3d6fb2ee3090e450c5cb6278
Fisherman-a <fisherman.a@gmail.com>
2020-03-31 19:48:08 +0200

    Eerste commit. aanmaken leeg project

---
+++ probeer/src/main/java/nl/test/probeer/ProbeerApplication.java
@@ -0,0 +1,13 @@
+package nl.test.probeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbeerApplication.class, args);
+	}
+
+}
---
+++ probeer/src/test/java/nl/test/probeer/ProbeerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.test.probeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
